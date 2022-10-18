4c1bd3fe5f7b8f704e2ba1017a39d7226e4fa62a
dennisappiahALX <appiahdennis1998@gmail.com>
2022-10-18 05:02:58 +0000

    dele

--- app.js
+++
@@ -1,9 +0,0 @@
-const Logger = require('./logger');
-const logger = new Logger();
-
-// Register a listener
-logger.on('messageLogged', (arg) => {
-    console.log('Listener called', arg);
-})
-
-logger.log('message kofi');
