764a8c2078a4c8b6622202c8ca7bf1ee88ea6734
dennisappiahALX <appiahdennis1998@gmail.com>
2022-10-20 15:28:14 +0000

    queries

--- mongodb-demo/filter_query.js
+++ mongodb-demo/filter_query.js
@@ -32,11 +32,14 @@ async function createCourse() {
 async function getCourses(){
     const courses = await Course
     // .find();   returns all courses in the database 
-    // .find({ author : 'Dennis', ispublished : true })  returns all courses by Dennis and is_published is True
-    // .find({ price: 10})    returns all price equal to 10
-    // .find({price : {$gt : 10}})   returns all price equal to 10
-    // .find({price : {$gt : 10, $lte : 20}})  returns all price between 10 and 20
-    // .find({price : {$in: [10, 15, 20]}})  returns price = 10 or 15, 20
+    .find({ author : 'Dennis', ispublished : true })  //returns all courses by Dennis and is_published is True
+    .find({ price: 10})    //returns all price equal to 10
+    .find({price : {$gt : 10}})   //returns all price equal to 10
+    .find({price : {$gt : 10, $lte : 20}})  //returns all price between 10 and 20
+    .find({price : {$in: [10, 15, 20]}})  //returns price = 10 or 15, 20
+    .find({ispublished : true, tags: {$in : ['backend', 'frontend']} }) //Get all published frontend or backend
+    .find({ispublished : true})  //get all published frontend and backend courses
+    .and([{ tags : 'backend'}, { tags : 'froentend'}])
     .limit(10)
     .sort({ name : 1})
     .select({ name: 1, tags : 1});
