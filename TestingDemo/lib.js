28aa81780f2047e7f8e9ac82b6484fd64658e875
dennisappiahALX <appiahdennis1998@gmail.com>
2022-10-27 12:23:54 +0000

    test files

---
+++ TestingDemo/lib.js
@@ -0,0 +1,48 @@
+const db = require('./db');
+const sendEmail = require('./mail')
+
+
+//Testing Numbers
+module.exports.absolute = function (number) {
+    if (number > 0) return number;
+
+    if (number < 0) return -number;
+
+    return 0;
+}
+
+
+//Testing strings
+
+module.exports.greet = function (name) {
+    return `Welcome ${name}`
+}
+
+//Testing arrays
+module.exports.getCurriencies = function( ) {
+    return ['US', 'EUR', 'AUD']
+}
+
+
+//Testing objects
+module.exports.getProduct = function(productId) {
+
+    return {id : productId, price:10}
+}
+
+//Testing exceptions
+module.exports.registerUser = function(username) {
+    if (!username) throw new Error('Username is not defined');
+
+    return {id: new Date().getTime(), username: username};
+}
+
+//Mock Functions
+module.exports.applyDiscount = function(order) {
+    const customer = db.getCustomerSync(order.id); 
+//in unit tests, we dont want to test the app compents 
+//with any extenal dependencies like database, http service
+
+    if (customer.point > 0)
+        order.totalprice *=0.9
+}
---
+++ TestingDemo/tests/lib.test.js
@@ -0,0 +1,105 @@
+const lib = require('../lib');
+const db = require('../db');
+
+describe('absolute', () => {
+    test('should return positive if input is positive', () => {
+        const result = lib.absolute(1);
+    
+        expect(result).toBe(1);
+    });
+    
+    test('should return negative if input is negative', () => {
+        const result = lib.absolute(-1);
+        
+        expect(result).toBe(1);
+    });
+
+    test('should return 0 if input is 0', () => {
+        const result = lib.absolute(0);
+        
+        expect(result).toBe(0);
+    });
+});
+
+//testing strings
+
+describe('greet', () => {
+    test('should return the greeting message', () => {
+        const result =lib.greet('Dennis');
+
+        expect(result).toBe('Welcome Dennis');
+        expect(result).toContain('Dennis');
+    })
+})
+
+//testings arrays
+
+describe('getCurriencies', () => {
+    test('should return array of currencies', () => {
+        const result =lib.getCurriencies();
+
+        //too general
+        expect(result).not.toBeNull();
+        expect(result).toBeDefined();
+
+        //too specific - the references of the numbers are matched
+
+        expect(result[0]).toBe('US');
+        expect(result[1]).toBe('EUR');
+        expect(result[2]).toBe('AUD');
+
+        // proper way
+        expect(result).toContain('US');
+        expect(result).toContain('EUR');
+        expect(result).toContain('AUD');
+
+    })
+})
+
+//testing objects
+describe('getProduct', () => {
+    test('return product with the given id', () => {
+        const result =lib.getProduct(1)
+
+        // expect(result).toEqual({id : 1, price:10});
+
+        expect(result).toMatchObject({id : 1, price:10});
+
+        expect(result).toHaveProperty('id', 1);
+
+    })
+})
+
+// Testing Exceptions
+describe('registerUser', () => {
+    test('should throw username is falsy', () => {
+    
+        const args = [null, NaN, undefined, 0, '', false]
+
+        args.forEach( a => {
+            expect(() => {lib.registerUser(a)}).toThrow();  
+        })
+
+    })
+
+    test('should return user object if username passed', () => {
+        const result = lib.registerUser('Dennis');
+
+        expect(result).toMatchObject({ username : 'Dennis'});
+        expect(result.id).toBeGreaterThan(0);
+    })
+})
+
+
+describe('applyDiscount', () => {
+    test('should apply 10% discount if customer has nore than 10points', () => {
+        db.getCustomerSync = function(customerId) {
+            console.log('Fake reading customer...');
+            return { id: customerId, points: 20}; 
+        }
+        let order = { customerId: 1, totalPrice: 9}
+        lib.applyDiscount(order);
+
+        expect(order.totalPrice).toBe(9);
+    })
+});
